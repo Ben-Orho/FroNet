19e10b9725deaf43cfcc39f07b18f739a54ad812
ALASHI1 <115797122+Muyiwa0@users.noreply.github.com>
2023-03-02 12:50:19 +0100

    fix

--- src/components/Scroll/CardScroll.jsx
+++ src/components/Scroll/CardScroll.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Fullpage, { FullPageSections, FullPageSection, FullPageNavigation} from "e@ap.cx/react-fullpage"
+// import React from 'react'
+// import Fullpage, { FullPageSections, FullPageSection, FullPageNavigation} from "e@ap.cx/react-fullpage"
 
-const CardScroll = () => {
-  return (
-    <div>
-        <Fullpage>
-          <FullPageSections>
-            <FullPageSection style={{height: "20vh"}}>
-              <h1>God is good</h1>
-            </FullPageSection>
-          </FullPageSections>
-        </Fullpage>
-    </div>
-  )
-}
+// const CardScroll = () => {
+// //   return (
+// //     <div>
+// //         <Fullpage>
+// //           <FullPageSections>
+// //             <FullPageSection style={{height: "20vh"}}>
+// //               <h1>God is good</h1>
+// //             </FullPageSection>
+// //           </FullPageSections>
+// //         </Fullpage>
+// //     </div>
+// //   )
+// }
 
-export default CardScroll
+// export default CardScroll
